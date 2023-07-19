d7ac8756825da5e5e45971b5cbc705f36fd59e1c
zephyrstorm123 <quitay.anthony@gmail.com>
2023-07-19 14:45:16 +0800

    first commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LittlelemonapicapstoneprojectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LittleLemonAPICapstoneProject'
